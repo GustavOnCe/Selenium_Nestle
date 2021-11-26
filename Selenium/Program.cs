febf8eb0219a0e95cae07786ece621edf9add0d1
GustavOnCe <gustavocontreras11@outlook.com>
2021-11-26 13:03:10 -0600

    Se comenta el codigo

--- Selenium/Program.cs
+++ Selenium/Program.cs
@@ -14,10 +14,11 @@ namespace Selenium
        
         static void Main(string[] args)
         {
-
+            
            //Se crea la lista viajes
             var viajes = new List<string>();
-           //Se asignan valores a la lista a travez de consola   
+            //Se asignan valores a la lista a travez de consola   
+            Console.Title = "NESTLE CARTA PORTE";
             Console.WriteLine("Ingrese el/los numeros de viaje");
             var i = 0;
             while (i < 100) {
@@ -65,10 +66,10 @@ namespace Selenium
                 Thread.Sleep(3000);
                 IWebElement viaje = controlador.FindElement(By.XPath("/html/body/div[2]/div/div/div/div/section/div[5]/div/div[2]/div/label/input"));
                 viaje.SendKeys(viajes[0 + x]);
-                Thread.Sleep(3000);
+                Thread.Sleep(1500);
                 IWebElement limpiar_viaje = controlador.FindElement(By.XPath("/html/body/div[2]/div/div/div/div/section/div[5]/div/div[2]/div/label/input"));
                 limpiar_viaje.Clear();
-                Thread.Sleep(3000);
+                Thread.Sleep(1500);
                 IWebElement clickBuscarC = controlador.FindElement(By.XPath("/html/body/div[2]/div/div/div/div/section/div[5]/button"));
                 clickBuscarC.Click();
                 Thread.Sleep(2000);
@@ -87,7 +88,11 @@ namespace Selenium
                 //Si el viaje no existe manda un mensaje a la consola y continua con los demas viajes
                 catch
                 {
-                    Console.WriteLine("Viaje {0} no encontrado. Favor de validarlo",viajes[x]);
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("\n Viaje {0} no encontrado. Favor de validarlo",viajes[x]);
                     //Si la lista no contiene mas viajes por descargar, finaliza el proceso
                     if (viajes.Count == 1)
                     {
@@ -140,75 +145,8 @@ namespace Selenium
 
            
 
-            /*var folder = new DirectoryInfo(@"C:\Users\Admin\Downloads\");
-            string from = @"C:\Users\Admin\Downloads\";
-            string to = @"C:\Users\Admin\Desktop\ejemplo\";
-
-            if (folder.Exists)
-            {
-                var files = folder.GetFiles(".csv");
-                files.ToList().ForEach(f => File.Move(from, to));
-            }*/
-
-            //int vueltas = viajes.Length;
-
 
             
-
-
-
-            /*string nombre_usuario;
-            Console.WriteLine("Por favor ingrese el usuario: ");
-            nombre_usuario = Console.ReadLine();
-
-            ChromeDriver controlador = new ChromeDriver();
-            controlador.Navigate().GoToUrl("http://201.163.75.133:83/");
-            controlador.Manage().Window.Maximize();
-
-            By Validador = By.ClassName("sorting_1");
-                                   
-            //search
-            IWebElement usuario = controlador.FindElement(By.XPath("/html/body/div/div/div/div[2]/form/div[1]/input"));
-            usuario.SendKeys("GCONTRERAS");
-            IWebElement contrasenia = controlador.FindElement(By.XPath("/html/body/div/div/div/div[2]/form/div[2]/input"));
-            contrasenia.SendKeys("GUSTA17");
-            IWebElement clickEntrar = controlador.FindElement(By.XPath("/html/body/div/div/div/div[2]/form/div[3]/input"));
-            clickEntrar.Click();
-            IWebElement clickSistemas = controlador.FindElement(By.XPath("/html/body/div/nav/div[1]/div[3]/div/ul/li[5]/a/span"));
-            clickSistemas.Click();
-            Thread.Sleep(2000);
-            IWebElement clickProgramas = controlador.FindElement(By.XPath("/html/body/div/nav/div[1]/div[3]/div/ul/li[5]/div/ul/li[2]/a/span"));
-            clickProgramas.Click();
-            IWebElement clickCrudUsuarios = controlador.FindElement(By.XPath("/html/body/div/nav/div[1]/div[3]/div/ul/li[5]/div/ul/li[2]/div/ul/li/a"));
-            clickCrudUsuarios.Click();
-            IWebElement clickNuevo = controlador.FindElement(By.XPath("/html/body/div/div/div[1]/div[1]/button"));
-            clickNuevo.Click();
-            IWebElement buscar_usuario = controlador.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div/div/form/div[1]/div[2]/div[2]/label/input"));
-            buscar_usuario.SendKeys(nombre_usuario);
-            Thread.Sleep(3000);
-            
-            if (controlador.FindElement(Validador).Displayed)
-            {
-                IWebElement clickUsuario = controlador.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div/div/form/div[1]/div[2]/div[3]/div[2]/table/tbody/tr"));
-                clickUsuario.Click();
-                Thread.Sleep(3000);
-                IWebElement clickGuardar = controlador.FindElement(By.XPath("/html/body/div[1]/div/div[2]/div/div/form/div[2]/button"));
-                clickGuardar.Click();
-                Thread.Sleep(3000);
-                Console.BackgroundColor = ConsoleColor.White;
-                Console.Clear();
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Console.WriteLine("\n Usuario {0} generado correctamente", nombre_usuario);
-                controlador.Close();
-            }
-            else
-            {
-               
-                controlador.Close();
-                Console.WriteLine("Usuario no encontrado");
-            }*/
-
 
         }
     }
